6b422ce689ef40197b8094423e389f446c433469
Gomosoft <jgomez@soluntech.com>
2015-03-12 16:30:29 -0500

    latest

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,21 +1,125 @@
  
 angular.module('soluntech')
-.controller('HomeCtrl', ['$scope', '$http', '$location', '$content',
-    function($scope, $http, $location, $content) {
-        console.log('Init Controller');
+.controller('HomeCtrl', ['$scope', '$http', '$location', '$content', '$sce',
+    function($scope, $http, $location, $content, $sce) {
+
            
             window.resizeMode();
 
             console.log($content);
 
-
            $content
            .posts()
            .get()
-           .success(function(rs){
+           .success(function(rs){            	 
             	 console.log(rs);
-           });
 
+            	 $scope.content = {}
+
+            	 for(x in rs)
+
+            	 	switch(rs[x].terms.category[0].name)
+            	       {
+            	       	   case "diseño a la medida":
+            	       	      $scope.content["customDesign"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+
+            	       	      $scope.content["customDesign"].content.content
+            	       	      .replace()
+
+            	       	      $scope.content["customDesign"].content.content = $sce.trustAsHtml($scope.content["customDesign"].content.content);
+            	       	   break;
+
+            	       	   case "clientes":
+            	       	      $scope.content["clientes"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+
+            	       	      $scope.content["clientes"].content.content = $sce.trustAsHtml($scope.content["clientes"].content.content);
+
+            	       	   break;	
+
+            	       	    case "soluciones":
+            	       	      $scope.content["soluciones"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+
+            	       	      $scope.content["soluciones"].content.content = $sce.trustAsHtml($scope.content["soluciones"].content.content);
+
+            	       	   break;	
+
+
+            	       	    case "contacto":
+            	       	      $scope.content["contacto"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+
+            	       	      $scope.content["contacto"].content.content = $sce.trustAsHtml($scope.content["contacto"].content.content);
+
+            	       	   break;	
+
+            	       	   case "apps":
+            	       	      $scope.content["apps"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+            	       	      $scope.content["apps"].content.content = $sce.trustAsHtml($scope.content["apps"].content.content);            	       	      
+            	       	   break;	
+
+            	       	   case "step1":
+            	       	      $scope.content["step1"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+            	       	      $scope.content["step1"].content.content = $sce.trustAsHtml($scope.content["step1"].content.content);            	       	      
+            	       	   break;	
+
+
+            	       	   case "step2":
+            	       	      $scope.content["step2"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+            	       	      $scope.content["step2"].content.content = $sce.trustAsHtml($scope.content["step2"].content.content);            	       	      
+            	       	   break;	
+
+            	       	   case "step3":
+            	       	      $scope.content["step3"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+            	       	      $scope.content["step3"].content.content = $sce.trustAsHtml($scope.content["step3"].content.content);            	       	      
+            	       	   break;	
+
+            	       	   case "step4":
+            	       	      $scope.content["step4"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+            	       	      $scope.content["step4"].content.content = $sce.trustAsHtml($scope.content["step4"].content.content);
+
+            	       	   break;	
+
+            	       	   case "step5":
+            	       	      $scope.content["step5"] = {
+            	       	      	  "title" : rs[x].title,
+            	       	      	  "content" : rs[x]
+            	       	      };
+            	       	      $scope.content["step5"].content.content = $sce.trustAsHtml($scope.content["step5"].content.content);            	       	      
+            	       	   break;	
+
+            	       }
+
+
+            	       console.log($scope.content);
+
+
+           });
 
     }]
 );
@@ -25,17 +129,14 @@ angular.module('soluntech')
 function contentApi($http) {
 
 
-    this.uri = undefined;
-    this.obj_key = undefined;
-    this.field_key = undefined;
+    this.uri = undefined;    
     this.base_url = 'http://54.84.73.92/wp/wp-json';
 
     this.posts = function() {
         this.uri = '/posts';
         return this;
     }
-    this.post = function(id) {
-        console.log(_obj_key);
+    this.post = function(id) {        
         this.post_id = id;
         this.uri = '/posts/' + this.post_id ;
         return this;
@@ -49,7 +150,9 @@ function contentApi($http) {
     //methods
 
     this.get = function() {
-        return (this.uri) ? $http.get(this.base_url + this.uri) : false;
+    	var uri = this.uri;
+    	this.reset;
+        return (uri) ? $http.get(this.base_url + uri) : false;
     }
 
 
